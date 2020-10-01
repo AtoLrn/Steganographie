6ec5315c7d18e515e77308eee73071a39e31f5e0
pc <gabdupety@hotmail.fr>
2020-10-01 17:50:23 +0200

    q

---
+++ index.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import cgi
+
+form = cgi.FieldStorage()
+print("Content-type: text/html; charset=utf-8\n")
+
+print(form.getvalue("name"))
+
+html = """<!DOCTYPE html>
+    <head>
+        <title>Steganography</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+"""
+
+print(html)
