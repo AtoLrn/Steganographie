7823b8f12388280734672dfa7cea901269a55a75
MacaronFR <denisft77@gmail.com>
2020-10-15 17:10:11 +0200

    bmp pret pour test (test a faire quand même)

--- bmp.py
+++ bmp.py
@@ -1,7 +1,8 @@
 import os
+import function
 
 file = open("test.bmp", "rb")
-newFile = open("testStegano.bmp", "wb")
+newFile = open("testStegano.bmp", "wb+")
 fileData = bytearray(file.read())
 pixelStart = fileData[10]
 width = fileData[21] * 16 * 16 * 16 + fileData[20] * 16 * 16 + fileData[19] * 16 + fileData[18]
@@ -10,13 +11,20 @@ fileLength = os.stat("test.bmp").st_size
 print(len(fileData))
 print(str(width) + " : " + str(heigth))
 data = "ABCD"
-for i in range(len(data)):
-	byte = ord(data[i]) * 2 # mise sur 9 bits de l'octet
-	if i == len(data) - 1:
-		byte += 1
-	for j in range(3):
-		tb = int(byte % 8)
-		byte = (byte - byte % 8) / 8
-		actualByte = pixelStart + i*3 + (3-j)
-		fileData[actualByte] = fileData[actualByte] - fileData[actualByte]%8 + tb
-newFile.write(fileData)
+# for i in range(len(data)):
+# 	byte = ord(data[i]) * 2 # mise sur 9 bits de l'octet
+# 	if i == len(data) - 1:
+# 		byte += 1
+# 	for j in range(3):
+# 		tb = int(byte % 8)
+# 		byte = (byte - byte % 8) / 8
+# 		actualByte = pixelStart + i*3 + (3-j)
+# 		fileData[actualByte] = fileData[actualByte] - fileData[actualByte]%8 + tb
+
+steganoFile = function.steganoBMP(fileData, pixelStart, data)
+newFile.write(steganoFile)
+
+fileData2 = newFile.read()
+
+print(function.steganoBMPReverse(fileData2, pixelStart))
+
