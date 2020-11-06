9876d1d9c3ed4d9aae8ab3178da83f62d2ce37ef
Gabriel <gabdupety@hotmail.fr>
2020-11-06 10:42:45 +0100

    Arborescence

---
+++ www-py/server.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+########################################################################
+import http.server
+########################################################################
+CURRENT_PATH = "/webpy/"
+WWW_DEFAULT_PORT = 8080
+WWW_DEFAULT_IP = ("51.178.50.244", WWW_DEFAULT_PORT)
+WWW_DEFAULT_PAGE = "index.py"
+WWW_PATH = "www-py/"
+WWW_DEFAULT_DOWLOAD_PATH = CURRENT_PATH+"download/"
+STEGANO = CURRENT_PATH+""
+########################################################################
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["VOIR L'ARCHITECTURE J'AI OUBLIE"]
+
+httpd = server(address, handler)
+
+print("serving at port", WWW_DEFAULT_PORT)
+httpd.serve_forever()
+
+
