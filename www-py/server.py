240e6cef05fea55e7a0ad7585169524d89664fc9
Gabriel <gabdupety@hotmail.fr>
2020-11-06 12:05:31 +0100

    SERVER-CGI

--- www-py/server.py
+++ www-py/server.py
@@ -7,7 +7,7 @@ import http.server
 ########################################################################
 CURRENT_PATH = "/webpy"
 WWW_DEFAULT_PORT = 8080
-WWW_DEFAULT_IP = ("51.178.50.244", WWW_DEFAULT_PORT)
+WWW_DEFAULT_IP = ("stegano.macaron-dev.fr/", WWW_DEFAULT_PORT)
 WWW_DEFAULT_PAGE = "/index.py"
 WWW_PATH = "/www-py"
 WWW_DEFAULT_DOWLOAD_PATH = CURRENT_PATH+"/download"
