b68acb4bc482ffa8c53f325ce995b4f8511f3012
Gabriel <gabdupety@hotmail.fr>
2020-11-06 12:03:32 +0100

    SERVER-CGI

--- www-py/server.py
+++ www-py/server.py
@@ -8,17 +8,16 @@ import http.server
 CURRENT_PATH = "/webpy"
 WWW_DEFAULT_PORT = 8080
 WWW_DEFAULT_IP = ("51.178.50.244", WWW_DEFAULT_PORT)
-WWW_DEFAULT_PAGE = "index.py"
+WWW_DEFAULT_PAGE = "/index.py"
 WWW_PATH = "/www-py"
-WWW_DEFAULT_DOWLOAD_PATH = CURRENT_PATH+"download/"
-STEGANO = CURRENT_PATH+""
+WWW_DEFAULT_DOWLOAD_PATH = CURRENT_PATH+"/download"
 
 ########################################################################
 server = http.server.HTTPServer
 handler = http.server.CGIHTTPRequestHandler
 handler.cgi_directories = [CURRENT_PATH+WWW_PATH]
 
-httpd = server(address, handler)
+httpd = server(WWW_DEFAULT_IP, handler)
 
 print("serving at port", WWW_DEFAULT_PORT)
 httpd.serve_forever()
