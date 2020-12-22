f9b98c3d929542900c79d6f178763a464ff4b3a1
Neutid <gabdupety@hotmail.fr>
2020-12-22 21:52:38 +0100

    Step1 : try to make working this bitch part2

--- template/server.py
+++ template/server.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 #coding:utf-8
-
+import http.server
 #################################
 CURRENT_PATH = "/webpy"
 WWW_DEFAULT_PORT = 8080
 WWW_DEFAULT_IP = ("", WWW_DEFAULT_PORT)             #all interfaces on the port listen == ""
-WWW_DEFAULT_PAGE = "/index.py"
 WWW_PATH = "/www_cgi-bin"
-WWW_DEFAULT_DOWLOAD_PATH = CURRENT_PATH+"/download"
 
 ########################################################################
-import http.server
 server = http.server.HTTPServer                     #classe du serveur HTTP
 handler = http.server.CGIHTTPRequestHandler         #classe du gestionnaire de requetes
 handler.cgi_directories = [WWW_PATH]
--- www_cgi-bin/index.py
+++ www_cgi-bin/index.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # coding: utf-8
 import cgi
-
-########################################################################
-CURRENT_PATH			    = "/webpy/"
-WWW_PATH			        = "www-py/"
-WWW_DEFAULT_PAGE 		    = CURRENT_PATH+WWW_PATH+"index.html"
-WWW_DEFAULT_DOWLOAD_PATH 	= CURRENT_PATH+"download/"
-########################################################################
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
