f0693b8f9565cc5df7d367ffe5037a8f8b341235
MacaronFR <denisft77@gmail.com>
2021-01-29 16:14:54 +0100

    fonction d'abstraction besoin d'un filename d'un message et sa longeur pour stegano, return -1 en erreur et 0 en succès. Besoin filename et retour message pur reverse

--- www-py/function.py
+++ www-py/function.py
@@ -1,4 +1,6 @@
-def steganoBMP(fileData, pixelStart, data):
+import os
+
+def steganoBMPFile(fileData, pixelStart, data):
 	for i in range(len(data)):
 		byte = ord(data[i]) * 2  # mise sur 9 bits de l'octet
 		if i == len(data) - 1:
@@ -12,7 +14,7 @@ def steganoBMP(fileData, pixelStart, data):
 	return fileData
 
 
-def steganoBMPReverse(fileData, pixelStart):
+def steganoBMPFileReverse(fileData, pixelStart):
 	byteValue = 0  # octet actuel
 	data = []
 	actualByte = pixelStart  # octet dans les données du BMP
@@ -26,3 +28,27 @@ def steganoBMPReverse(fileData, pixelStart):
 		actualByte += 1  # déplacement de l'octet a lire (car pixel RGB+0xFF en BMP)
 	return data
 
+
+def steganoBMP(filename, message, length):
+	f = open(filename, "rb")
+	fData = bytearray(f.read())
+	f.close()
+	pS = fData[10]
+	width = fData[21] * 16 * 16 * 16 + fData[20] * 16 * 16 + fData[19] * 16 + fData[18]
+	heigth = fData[25] * 16 * 16 * 16 + fData[24] * 16 * 16 + fData[23] * 16 + fData[22]
+	if length > ((width * length) / 3):
+		print("Error message length too long for this file")
+		return -1
+	newData = steganoBMPFile(fData, pS, message)
+	f = open("result.bmp", "wb")
+	f.write(newData)
+	f.close()
+	return 0
+
+
+def steganoBMPReverse(filename):
+	f = open(filename, "rb")
+	fData = bytearray(f.read())
+	f.close()
+	pS = fData[10]
+	return steganoBMPFileReverse(fData, pS)
