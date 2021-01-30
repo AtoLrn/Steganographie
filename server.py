309e37a63ea51b127733a96f47f32dbe705462a1
Neutid <>
2021-01-30 19:20:26 +0100

    coquille import server

--- server.py
+++ server.py
@@ -4,20 +4,20 @@
 ########################################################################
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
-import router
+import cgi_bin.router
 import urllib.parse
 import os                                   #permet ainsi de gérer l’arborescence des fichiers
 
 ########################################################################
-WWW_DEFAULT_PORT = 8080
-SERVER = ("", WWW_DEFAULT_PORT)             #all interfaces on the port listen == ""
+DEFAULT_PORT = 8080
+HOSTNAME = ("", DEFAULT_PORT)               #all interfaces on the port listen == ""
 WWW_PATH = "/cgi-bin/"
 
 
 ########################################################################
 class ServerPY(BaseHTTPRequestHandler):
     def do_GET(self):
-        response = router.route_get(self.path)
+        response = cgi_bin.router.route_get(self.path)
         if response['code'] != 404:
             self.send_response(200)
             self.send_header("Content-type", response['type'])
@@ -38,11 +38,14 @@ class ServerPY(BaseHTTPRequestHandler):
 #########################################################################
 handler = ServerPY                                      #classe du gestionnaire de requetes
 handler.cgi_directories = [WWW_PATH]
-httpd = HTTPServer(SERVER, handler)                     #instancie le serveur (addr + gestionnaire)
+httpd = HTTPServer(HOSTNAME, handler)                   #instancie le serveur (addr + gestionnaire)
 
 ########################################################################
+print(time.asctime(), "Server Starts - %s:%s" % (HOSTNAME, DEFAULT_PORT))
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
     pass
 
+httpd.server_close()
+print(time.asctime(), "Server Stop %s:%s" % (HOSTNAME, DEFAULT_PORT))
