ba8dd0130485c318848f1bfaefb28878a80cca66
MacaronFR <denisft77@gmail.com>
2021-01-30 18:15:18 +0100

    supression ligne inutile

--- cgi-bin/bmp.py
+++ cgi-bin/bmp.py
@@ -1,16 +1,6 @@
 import function
 
 data = "bonjour bande de con"
-# for i in range(len(data)):
-# 	byte = ord(data[i]) * 2 # mise sur 9 bits de l'octet
-# 	if i == len(data) - 1:
-# 		byte += 1
-# 	for j in range(3):
-# 		tb = int(byte % 8)
-# 		byte = (byte - byte % 8) / 8
-# 		actualByte = pixelStart + i*3 + (3-j)
-# 		fileData[actualByte] = fileData[actualByte] - fileData[actualByte]%8 + tb
-
 function.steganoBMP("test.bmp", data, 52)
 res = function.steganoBMPReverse("result.bmp")
 for i in range(len(res)):
