0eea455efbd439d16939ced1db80d440f8fa5ded
Neutid <>
2021-01-31 19:52:30 +0100

    Application de fonctions (building)

--- cgi_bin/router.py
+++ cgi_bin/router.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3.7
 
+import cgi
+import cgi_bin.function
+import os
+
 ########################################################################
+
 
 def route_get(URI):
     if URI == "/":
@@ -11,9 +16,38 @@ def route_get(URI):
 
 
 def route_post(URI):
+    form = cgi.FieldStorage()
+    image = form['img']
+    ext = os.path.splitext(image)
     if URI == "/stegano":
-        img = bytes(open("template/test.html", "r").read(), "utf-8")
-        return {"code": 200, "file": img, "type": "text/html"}
+        msg = form['msg']
+        pwd = form['pwd']
+
+        if ext == ".bmp":
+            #traitement fonction bmp
+            return {"code": 200, "file": image, "type": "image/bmp"}
+
+        else if ext == ".png":
+            #traitement fonction png
+            return {"code": 200, "file": image, "type": "image/png"}
+
+        else:
+            return {"code": 404}
+
+    else if URI == "/stegano_reverse":
+        img = form['img']
+        pwd = form['pwd']
+
+        if ext == ".bmp":
+            #traitement fonction bmp
+            return {"code": 200, "file": msg, "type": "text/html"}
+
+        else if ext == ".png":
+            msg = function.decodePng(image)
+            return {"code": 200, "file": msg, "type": "text/html"}
+
+        else:
+            return {"code": 404}
+
     else:
         return {"code": 404}
-
